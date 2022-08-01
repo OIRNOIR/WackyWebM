95b7fc08442747135088d263165a9adcfe38f397
ArtificialButter <59352535+artificialbutter@users.noreply.github.com>
2022-07-31 23:52:07 -0700

    Remove list of modes

--- run.bat
+++ run.bat
@@ -13,7 +13,7 @@ REM Print Ascii Art
 for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 
 REM Ask for required inputs
-set /p "mode=Mode (Bounce, Shutter, Bounce+Shutter, Sporadic): "
+set /p "mode=Mode: "
 set /p "file=File: "
 
 REM Run node commmand
