0c142d6655ff4ab4f4efbc326e1ec98aec85ec57
OIRNOIR <oirnoir@outlook.com>
2022-08-02 04:12:25 -0700

    Update mode lists

--- run.bat
+++ run.bat
@@ -13,7 +13,7 @@ REM Print Ascii Art
 for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 
 REM Ask for required inputs
-set /p "mode=Mode (Bounce, Shutter, Sporadic, Bounce+Shutter, Shrink, Audio-Bounce, Audio-Shutter, Audio-Both): "
+set /p "mode=Mode (Bounce, Shutter, Sporadic, Shrink, AudioBounce, AudioShutter, AudioBoth, Keyframes): "
 set /p "file=File: "
 
 REM Run node commmand
