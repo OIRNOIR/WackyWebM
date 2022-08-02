bb8b748369c8e189639d6b94d178dd407cf7c4f0
T Brieger <tilman.brieger@gmx.de>
2022-08-02 11:47:41 +0200

    fix both the audio modes
    (missing await + destructuring assignment)

--- modes/audiobounce.js
+++ modes/audiobounce.js
@@ -2,12 +2,12 @@ const util = require('../util.js')
 let audioMap
 let audioMapL
 module.exports = {
-	setup: (info) => {
-		audioMap = util.getAudioLevelMap(info.videoPath)
+	setup: async info => {
+		audioMap = await util.getAudioLevelMap(info.videoPath)
 		audioMapL = audioMap.length - 1
 	},
 	getFrameBounds: (info) => {
-		const percentMax = audioMap[Math.max(Math.min(Math.floor((info.frame / (info.frameCount - 1)) * audioMapL), audioMapL), 0)]
+		const { percentMax } = audioMap[Math.max(Math.min(Math.floor((info.frame / (info.frameCount - 1)) * audioMapL), audioMapL), 0)]
 		return {
 			height: info.frame === 0 ? info.maxHeight : Math.max(Math.floor(Math.abs(info.maxHeight * percentMax)), util.delta),
 		}
