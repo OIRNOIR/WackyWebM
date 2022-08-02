d1f137dd9d98f233201f3d23057e1d0f7cb3729c
q <69131802+earthmite@users.noreply.github.com>
2022-08-02 03:02:56 -0700

    Create jumpscare.js

---
+++ modes/jumpscare.js
@@ -0,0 +1,12 @@
+module.exports = {
+	setup: () => {},
+	getFrameBounds: (info) => ({
+		height = Math.max(1, Math.floor(maxHeight - (index / (decimalFramerate / 10)) * Math.PI))
+					width = Math.max(1, Math.floor(maxWidth - (index / (decimalFramerate / 10)) * Math.PI))
+					if (index > 466)
+		{
+			height = maxHeight;
+			width = maxWidth;
+		}	
+	}),
+}
