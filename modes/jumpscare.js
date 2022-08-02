b6252a09932b5d67a39a72ca04f408b05ae7ebef
q <69131802+earthmite@users.noreply.github.com>
2022-08-02 03:04:43 -0700

    Update jumpscare.js

--- modes/jumpscare.js
+++ modes/jumpscare.js
@@ -3,10 +3,11 @@ module.exports = {
 	getFrameBounds: (info) => ({
 		height = Math.max(1, Math.floor(maxHeight - (index / (decimalFramerate / 10)) * Math.PI))
 					width = Math.max(1, Math.floor(maxWidth - (index / (decimalFramerate / 10)) * Math.PI))
-					if (index > 466)
+					if (index > 466) //change 466 to the specific frame you want to become full size
 		{
 			height = maxHeight;
 			width = maxWidth;
 		}	
 	}),
 }
+//
