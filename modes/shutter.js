20fa6db086c631908c5dd7043004a914f492b54d
T Brieger <tilman.brieger@gmx.de>
2022-08-23 02:51:20 +0200

    automatically set "delta" based on video size

--- modes/shutter.js
+++ modes/shutter.js
@@ -1,9 +1,8 @@
 'use strict'
 
-const util = require('../util.js')
 module.exports = {
 	setup: () => {},
 	getFrameBounds: (info) => ({
-		width: info.frame === 0 ? info.maxWidth : Math.floor(Math.abs(Math.cos((info.frame / (info.frameRate / info.tempo)) * Math.PI) * (info.maxWidth - util.delta))) + util.delta,
+		width: info.frame === 0 ? info.maxWidth : Math.floor(Math.abs(Math.cos((info.frame / (info.frameRate / info.tempo)) * Math.PI) * info.maxWidth)),
 	}),
 }
