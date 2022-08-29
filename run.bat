4c00ecae5caa93091e079d133d13b1b9f589bd59
T Brieger <tilman.brieger@gmx.de>
2022-08-29 16:56:09 +0200

    Improve Docs and Wrapper Scripts (#146)
    * improve documentation
    
    * make windows wrapper script check for dependencies
    
    * make windows wrapper script handle npm error
    
    * apply improvements to non-windows wrapper as well
    
    * Keyframe Documentation & minor bugfix
    
    * english is hard

--- run.bat
+++ run.bat
@@ -1,18 +1,35 @@
 @echo off
- 
-::: __          __        _       __          __  _     __  __ 
-::: \ \        / /       | |      \ \        / / | |   |  \/  |
-:::  \ \  /\  / /_ _  ___| | ___   \ \  /\  / /__| |__ | \  / |
-:::   \ \/  \/ / _` |/ __| |/ / | | \ \/  \/ / _ \ '_ \| |\/| |
-:::    \  /\  / (_| | (__|   <| |_| |\  /\  /  __/ |_) | |  | |
-:::     \/  \/ \__,_|\___|_|\_\\__, | \/  \/ \___|_.__/|_|  |_|
-:::                             __/ |                          
-:::                            |___/                           
 
-REM Print Ascii Art
+:: Print Ascii Art (ugly because batch requires escaping of "|" and "<" with `^`, instead of with `\`.)
+
+echo __          __        _       __          __  _     __  __
+echo \ \        / /       ^| ^|      \ \        / / ^| ^|   ^|  \/  ^|
+echo  \ \  /\  / /_ _  ___^| ^| ___   \ \  /\  / /__^| ^|__ ^| \  / ^|
+echo   \ \/  \/ / _` ^|/ __^| ^|/ / ^| ^| \ \/  \/ / _ \ '_ \^| ^|\/^| ^|
+echo    \  /\  / (_^| ^| (__^|   ^<^| ^|_^| ^|\  /\  /  __/ ^|_) ^| ^|  ^| ^|
+echo     \/  \/ \__,_^|\___^|_^|\_\\__, ^| \/  \/ \___^|_.__/^|_^|  ^|_^|
+echo                             __/ ^|
+echo                            ^|___/
+
+echo.
+
+:: test for dependencies.
+echo Testing for FFmpeg, FFprobe, Node and npm...
+
+where /q ffmpeg || echo FFmpeg could not be found && exit /B
+where /q ffprobe || echo FFprobe could not be found && exit /B
+where /q node || echo Node could not be found && exit /B
+where /q npm || echo npm could not be found && exit /B
 
 echo Installing Dependencies - this might take a while the first time.
-call npm i
+call npm i >NUL 2>npm.log || (
+  echo Issue installing dependencies using npm.
+  echo Error:
+  type npm.log
+  del npm.log
+  echo Exiting...
+  exit /b
+)
 
 echo Enter the language you want to use, or press enter for the default (english)
 set /p "lang= "
