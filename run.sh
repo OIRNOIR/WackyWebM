acf9ad329a7a428fcb9f404bfc9789e9e5cb1345
T Brieger <tilman.brieger@gmx.de>
2022-08-29 16:56:09 +0200

    Improve Docs and Wrapper Scripts (#146)
    * improve documentation
    
    * make windows wrapper script check for dependencies
    
    * make windows wrapper script handle npm error
    
    * apply improvements to non-windows wrapper as well
    
    * Keyframe Documentation & minor bugfix
    
    * english is hard

--- run.sh
+++ run.sh
@@ -1,18 +1,33 @@
 #!/bin/sh
 # Print Ascii Art
-echo " 
- __          __        _       __          __  _     __  __ 
+echo "
+ __          __        _       __          __  _     __  __
  \ \        / /       | |      \ \        / / | |   |  \/  |
   \ \  /\  / /_ _  ___| | ___   \ \  /\  / /__| |__ | \  / |
    \ \/  \/ / _\` |/ __| |/ / | | \ \/  \/ / _ \ '_ \| |\/| |
     \  /\  / (_| | (__|   <| |_| |\  /\  /  __/ |_) | |  | |
-     \/  \/ \__,_|\___|_|\_\\\\\__, | \/  \/ \___|_.__/|_|  |_|
-                             __/ |                          
-                            |___/                           
+     \/  \/ \__,_|\___|_|\_\\\\__, | \/  \/ \___|_.__/|_|  |_|
+                             __/ |
+                            |___/
 "
 
+echo "Testing for FFmpeg, FFprobe, Node and npm..."
+if ! command -v ffmpeg &>/dev/null; then echo "FFmpeg not found, exiting..." && exit 1; fi
+if ! command -v ffprobe &>/dev/null; then echo "FFprobe not found, exiting..." && exit 1; fi
+if ! command -v node &>/dev/null; then echo "Node not found, exiting..." && exit 1; fi
+if ! command -v npm &>/dev/null; then echo "npm not found, exiting..." && exit 1; fi
+echo "No issues found!"
+
 echo "Installing Dependencies - this might take a while the first time."
-npm i
+if ! npm i >/dev/null 2>npm.log
+then
+  echo "Error during dependency installation using npm"
+  echo "Issue description:"
+  cat npm.log
+  rm npm.log
+  echo "Exiting..."
+  exit 1
+fi
 
 echo "Enter the language you want to use, or press enter for the default (english)"
 read lang
