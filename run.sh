1859b44c67fd45fb3a4a92bf5a14eb47508e504d
T Brieger <tilman.brieger@gmx.de>
2023-01-06 13:42:28 +0100

    slightly improve wrapper scripts
    - show all the missing tools instead of only the first
    - wait for user confirmation after erroring out
    - add argument to skip tool check

--- run.sh
+++ run.sh
@@ -11,12 +11,19 @@ echo "
                             |___/
 "
 
-echo "Testing for FFmpeg, FFprobe, Node and npm..."
-if ! command -v ffmpeg &>/dev/null; then echo "FFmpeg not found, exiting..." && exit 1; fi
-if ! command -v ffprobe &>/dev/null; then echo "FFprobe not found, exiting..." && exit 1; fi
-if ! command -v node &>/dev/null; then echo "Node not found, exiting..." && exit 1; fi
-if ! command -v npm &>/dev/null; then echo "npm not found, exiting..." && exit 1; fi
-echo "No issues found!"
+function checktools
+{
+	echo "Testing for FFmpeg, FFprobe, Node and npm..."
+	alldependencies=1
+	if ! command -v ffmpeg &>/dev/null; then echo "FFmpeg not found" && alldependencies=0; fi
+	if ! command -v ffprobe &>/dev/null; then echo "FFprobe not found" && alldependencies=0; fi
+	if ! command -v node &>/dev/null; then echo "Node not found" && alldependencies=0; fi
+	if ! command -v npm &>/dev/null; then echo "npm not found" && alldependencies=0; fi
+
+	if [ "$alldependencies" = "0" ]; then echo "If you believe this to be an error, run \"./run.sh --notoolcheck\"." && read -p "Press any key to exit." -n 1 -r && exit 1; fi
+}
+
+if [ "$1" != "--notoolcheck" ]; then checktools && echo "No issues found!"; else echo "Skipping Tool check..."; fi
 
 echo "Installing Dependencies - this might take a while the first time."
 if ! npm i >/dev/null 2>npm.log
